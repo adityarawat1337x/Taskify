e4a2814b3e0a877bf503442706b2c70c7d76874f
napster <napster98766@gmail.com>
2022-02-22 16:36:29 +0530

    heroku hosting started

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path")
 const express = require("express")
 const colors = require("colors")
 const dotenv = require("dotenv").config()
@@ -19,6 +20,22 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use("/api/task", require("./routes/taskRoute"))
 app.use("/api/user", require("./routes/userRoute"))
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")))
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  )
+} else {
+  app.get("*", (req, res) => {
+    res.send(
+      "Hello from development... set your enviroment variable to production"
+    )
+  })
+}
+
 app.use(taskErrorHandler)
 
 app.listen(port, () => {
